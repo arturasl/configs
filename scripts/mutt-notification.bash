c7672efa7bc36e1a8c25c5d99638c3c0ef6da433
Artūras Lapinskas <arturaslape@gmail.com>
2013-04-12 10:47:06 +0300

    updated mutt notifications to work with cacoaDialog

--- scripts/mutt-notification.bash
+++ scripts/mutt-notification.bash
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+source "$(dirname "$0")/util.bash"
+
 echo "$1"
 have_new=$(echo "$1" | grep 'New:')
 
 if [ -n "$have_new" ]; then
-	mails=$(echo "$1" | sed -e 's/.*New:\([[:digit:]]\+\).*/\1/g' ) # number of new messages
-	notify-send -u normal -t 10000 'New mail' "New mails: $mails"
+	mails=$(echo "$1" | sed -e 's/.*New:[[:space:]]*\([[:digit:]]*\).*/\1/g' ) # number of new messages
+	msg="New mails: $mails"
+	title='New mail'
+	if [ -x /Applications/CocoaDialog.app/Contents/MacOS/CocoaDialog ]; then
+		/Applications/CocoaDialog.app/Contents/MacOS/CocoaDialog bubble --timeout 10 --text "$msg" --title "$title" &
+	elif utilCommandExists 'notify-send'; then
+		notify-send -u normal -t 10000 "$title" "$msg"
+	else
+		utilShowError 'Could not show mutt notification'
+	fi
 fi
