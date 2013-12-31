e5395ebbfc385a7f07a7d810948a74aa48dd5b1b
Artūras Lapinskas <arturaslape@gmail.com>
2013-12-31 13:00:26 +0200

    slate

---
+++ slate.js
@@ -0,0 +1,19 @@
+/*global
+slate: false
+*/
+'use strict';
+
+slate.bindAll({
+	'l:cmd' : slate.op('focus', { direction : 'right' }),
+	'h:cmd' : slate.op('focus', { direction : 'left' }),
+	'k:cmd' : slate.op('focus', { direction : 'up' }),
+	'j:cmd' : slate.op('focus', { direction : 'down' }),
+
+	'l:cmd,shift' : slate.op('resize', { width : '+10' }),
+	'h:cmd,shift' : slate.op('resize', { width : '+10', anchor: 'bottom-right' }),
+	'k:cmd,shift' : slate.op('resize', { height : '+10', anchor: 'bottom-right'}),
+	'j:cmd,shift' : slate.op('resize', { height : '+10'}),
+
+	'q:cmd' : slate.op('hint', { characters : 'asdfghjkl' }),
+	'g:cmd' : slate.op('grid', {}),
+});
