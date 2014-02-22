3ba6d8152afae3934ce3dea56da5b0052bca75b2
Artūras Lapinskas <arturaslape@gmail.com>
2014-02-22 16:29:09 +0200

    using alt as meta key

--- slate.js
+++ slate.js
@@ -4,17 +4,17 @@ slate: false
 'use strict';
 
 slate.bindAll({
-	'l:cmd' : slate.op('focus', { direction : 'right' }),
-	'h:cmd' : slate.op('focus', { direction : 'left' }),
-	'k:cmd' : slate.op('focus', { direction : 'up' }),
-	'j:cmd' : slate.op('focus', { direction : 'down' }),
+	'l:alt' : slate.op('focus', { direction : 'right' }),
+	'h:alt' : slate.op('focus', { direction : 'left' }),
+	'k:alt' : slate.op('focus', { direction : 'up' }),
+	'j:alt' : slate.op('focus', { direction : 'down' }),
 
-	'l:cmd,shift' : slate.op('resize', { width : '+10' }),
-	'h:cmd,shift' : slate.op('resize', { width : '+10', anchor: 'bottom-right' }),
-	'k:cmd,shift' : slate.op('resize', { height : '+10', anchor: 'bottom-right'}),
-	'j:cmd,shift' : slate.op('resize', { height : '+10'}),
+	'l:alt,shift' : slate.op('resize', { width : '+10' }),
+	'h:alt,shift' : slate.op('resize', { width : '+10', anchor: 'bottom-right' }),
+	'k:alt,shift' : slate.op('resize', { height : '+10', anchor: 'bottom-right'}),
+	'j:alt,shift' : slate.op('resize', { height : '+10'}),
 
-	'q:cmd' : slate.op('hint', { characters : 'asdfghjkl' }),
-	'g:cmd' : slate.op('grid', {}),
-	'a:cmd' : slate.op('move', {x : 'screenOriginX', y : 'screenOriginY', width : 'screenSizeX', height : 'screenSizeY' }),
+	'q:alt' : slate.op('hint', { characters : 'asdfghjkl' }),
+	'g:alt' : slate.op('grid', {}),
+	'a:alt' : slate.op('move', {x : 'screenOriginX', y : 'screenOriginY', width : 'screenSizeX', height : 'screenSizeY' }),
 });
