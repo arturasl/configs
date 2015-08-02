a4a4a1dde6d563c35ab0c400788147f4840f8cea
arturasl <589741+arturasl@users.noreply.github.com>
2015-08-02 20:19:05 +0300

    show integer part of temperature in topbar

--- dwm/topbar/weather/forecast.py
+++ dwm/topbar/weather/forecast.py
@@ -99,4 +99,4 @@ if args.forecast:
         for data in day_data[1]:
             print('{:2d}h {:2.0f}° - {}'.format(data['time'].hour, data['temp'], data['text'].lower()))
 else:
-    print('{} {}°'.format(data['position']['place'], closest_forecast_data['temp']), end = '')
+    print('{} {:.0f}°'.format(data['position']['place'], closest_forecast_data['temp']), end = '')
