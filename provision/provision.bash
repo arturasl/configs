4668f2b1b012c38d199d07d65702407b2094728c
Artūras Lapinskas <arturaslape@gmail.com>
2015-08-21 22:57:24 +0300

    added boost libraries as c dependency

--- provision/provision.bash
+++ provision/provision.bash
@@ -22,7 +22,7 @@ $apti openjdk-{6,7}-jdk        # java
 $apti nodejs npm \
 	&& sudo npm install -g jslint csslint uglifycss uglify-js # javascript
 $apti texlive-full             # latex
-$apti astyle cppcheck          # c
+$apti astyle cppcheck libboost-all-dev  # c
 ## }}
 ## idea {{
 $apti geany
