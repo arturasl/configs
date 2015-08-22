19de7626e7a856d8323f592fbd76199a08081489
arturasl <589741+arturasl@users.noreply.github.com>
2015-08-22 15:58:35 +0300

    fix gnome keyring not passing all environment variables

--- dwm/before.bash
+++ dwm/before.bash
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# fixed https://bugs.launchpad.net/ubuntu/+source/gnome-keyring/+bug/932177
+if [ -n "$GNOME_KEYRING_PID" ]; then
+    eval $(gnome-keyring-daemon --start)
+    export GNOME_KEYRING_CONTROL
+    export SSH_AUTH_SOCK
+    export GPG_AGENT_INFO
+fi
