b0bda839d38b6e9a33a50d02ade4acb151972d84
arturasl <589741+arturasl@users.noreply.github.com>
2015-08-22 15:59:30 +0300

    changed alias file format - now includes comments and multiple emails

--- scripts/mutt-alias-query.bash
+++ scripts/mutt-alias-query.bash
@@ -2,14 +2,21 @@
 
 echo 'Using custom query command'
 
-# expected format: email\tlong name\tdescription
+# mutt expects format: email\tlong name\tdescription
 query=$(echo -n "$1" | awk '{ printf "%s", tolower($0) }')
 awk -- '
 	/^[[:space:]]*$/ { next } # skip empty lines
 	/^#/ { sub(/^#[[:space:]]*/, "", $0); group = $0; next } # capture text after "#" in group variable
 	tolower($0) ~ /'$query'/ {
-		printf "%s\t", $NF
-		for (i = 3; i < NF; i += 1) printf "%s%s", (i == 3) ? "" : " ", $i
-		printf "\t%s\n", group
+		long_name=""
+		email=""
+		for (i = 1; i <= NF; i += 1) {
+			if (index($i, "@") > 0) {
+				email = $i
+				break
+			}
+			long_name = long_name (i == 1 ? "" : " ") $i
+		}
+		printf "%s\t%s\t%s\n", email, long_name, group
 	}
 ' ~/.mutt-alias | sort
--- scripts/mutt-message-filter.py
+++ scripts/mutt-message-filter.py
@@ -17,34 +17,27 @@ addresses = email.utils.getaddresses(parsed_email.get_all('to', [])
 def decode_header(hdr):
     de = email.header.decode_header(hdr)
     return ''.join(el[0] if not el[1] else el[0].decode(el[1]) for el in de)
-addresses = [tuple(decode_header(e) for e in addr) for addr in addresses if addr]
-# normalize emails/names and remove anything that do not looks like an email
-addresses = [(re.sub('[\'"]', '', addr[0].title()), addr[1].lower()) for addr in addresses if '@' in addr[1]]
-# add alias names
+
 addresses = [
-        (
-            re.sub(r'[^a-z_.0-9]',
-                '',
-                unicodedata
-                    .normalize('NFKD', addr[0])
-                    .encode('ascii', 'ignore')
-                    .decode('ascii')
-                    .replace(' ', '.')
-                    .lower() if addr[0] else addr[1].split('@')[0])
-            , addr[0]
-            , addr[1]
-        )
-        for addr in addresses]
+        tuple(decode_header(e) for e in addr)
+        for addr in addresses
+        if addr]
+
+# normalize emails/names and remove anything that do not looks like an email
+addresses = [
+        (re.sub('[\'"]', '', addr[0].title()), addr[1].lower())
+        for addr in addresses
+        if '@' in addr[1]]
 
 alias_file = os.path.expanduser('~/Dropbox/configs/mutt/mutt-alias')
 # remove addresses that are in alias file
 with open(alias_file, 'r') as aliasses_fp:
     for line in aliasses_fp:
-        if line and line[0] != '#':
-            addresses = [addr for addr in addresses if addr[2] not in line]
+        if line and not re.match(r'^[ #]', line):
+            addresses = [addr for addr in addresses if addr[1] not in line]
 
 # append new addreses
 if addresses:
     with open(alias_file, 'a') as aliasses_fp:
         for addr in addresses:
-            aliasses_fp.write('alias {}\n'.format(' '.join(addr)))
+            aliasses_fp.write('{}\n'.format(' '.join(addr)))
