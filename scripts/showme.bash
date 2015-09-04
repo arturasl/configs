65b8a032091eb4f463a6d4023f80af2145e94948
Artūras Lapinskas <arturaslape@gmail.com>
2015-09-04 23:27:54 +0300

    better information for video files

--- scripts/showme.bash
+++ scripts/showme.bash
@@ -42,10 +42,15 @@ capCat() {
 	fi
 }
 
+capVideoInfo() {
+	{ program='mediainfo' && utilCommandExists "$program" && echo "$program"; } \
+	|| { program='file' && utilCommandExists "$program" && echo "$program"; }
+}
+
 # regex for file name \t regex for mime type \t command for viewing gui \t command for viewing as text stream \t priority
 read -r -d '' capabilities <<EOF
 ^https?://|^ftps?://|\.html?$|^www\.	text/html	capWebBrowser	pandoc -f html -t markdown	50
-\.mp[3-4]$|\.m4v$|\.flv|\.mov|\.avi|\.ogv|\.wmv|\.mkv|\.3gp	video/.+|audio/.+	mplayer	file	50
+\.mp[3-4]$|\.m4v$|\.flv|\.mov|\.avi|\.ogv|\.wmv|\.mkv|\.3gp	video/.+|audio/.+	mplayer	capVideoInfo	50
 \.pdf|\.eps|\.ps	application/pdf	capPDFViewer	pdftotext FILENAME -	50
 \.jpe?g$|\.png$|\.gif$	image/.+	feh --draw-actions	cacaview	50
 \.docx?|\.odt|\.ods|\.xlsx?	application/(vnd.openxml.*|.*excel.*|.*oasis.*)	capWordViewer		50
