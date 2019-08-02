a09e5d42fcdaf7a80cea3a102e38bb0721c6a561
Arturas Lapinskas <arturaslape@gmail.com>
2019-08-02 12:11:43 +0200

    Update dwm.

--- provision/build/dwm/dwm.bash
+++ provision/build/dwm/dwm.bash
@@ -7,15 +7,13 @@ rm -rf "${build_dir}" && mkdir -p "${build_dir}" && cd "${build_dir}"
 
 # download and patch
 
-wget -O dwm.tar.gz http://dl.suckless.org/dwm/dwm-6.0.tar.gz
-echo '35346f873a27f219ae266594b3690407f95d06ef  dwm.tar.gz' | sha1sum --status --check -
+wget -O dwm.tar.gz https://dl.suckless.org/dwm/dwm-6.2.tar.gz
 tar xzf dwm.tar.gz && mv dwm-*/* . && rm -rf dwm.tar.gz
 
-wget -O systray.diff http://dwm.suckless.org/patches/dwm-6.0-systray.diff
+wget -O systray.diff https://dwm.suckless.org/patches/systray/dwm-systray-20190208-cb3f58a.diff
 patch -i systray.diff
 
-wget -O pertag.patch http://dwm.suckless.org/patches/dwm-6.1-pertag.diff
-echo '037f961c447c6278bba6e4514668b6b0c7a3f8f6  pertag.patch' | sha1sum --status --check -
+wget -O pertag.patch https://dwm.suckless.org/patches/pertag/dwm-pertag-20170513-ceac8c9.diff
 patch -i pertag.patch
 
 # diff --show-c-function -u --ignore-blank-lines ~/Builds/dwm/config.def.h config.h > config.h.patch
