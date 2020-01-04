0c6426750316565966e2b28cb95d1a1ee42139ad
arturasl <589741+arturasl@users.noreply.github.com>
2020-01-04 09:45:15 +0100

    Remove delay for entering escape sequences.

--- shells/config.fish
+++ shells/config.fish
@@ -3,3 +3,8 @@ set --export --global CDPATH .
 if status --is-interactive
 	~/configs/shells/common.bash fish | source
 end
+
+# Prevent a delay after escaping.
+# This is useful for vim mode where we want to go from input mode to normal
+# as fast as possible.
+set -g fish_escape_delay_ms 10
