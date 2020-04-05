d920205e6688cf218ee2f7a90066afa88679133d
Arturas Lapinskas <arturaslape@gmail.com>
2020-04-05 12:42:06 +0200

    Move fish commands from common to fish specific config.

--- shells/config.fish
+++ shells/config.fish
@@ -1,19 +1,41 @@
 set --export --global CDPATH .
+set --export --global fish_greeting '' # do not show greeting message.
 
 if status --is-interactive
 	~/configs/shells/common.bash fish | source
 end
 
-# Use ctrn-{n,p,o} to move over history.
-bind --mode insert \cn history-search-backward
-bind --mode insert \cp history-search-forward
-bind --mode insert \co accept-autosuggestion
+function fish_prompt
+	if [ $status -ne 0 ]
+		set_color red
+	end
+	echo -n '$ '
+	set_color normal
+end
+
+function fish_user_key_bindings
+	fish_vi_key_bindings
+end
 
 # Prevent a delay after escaping.
 # This is useful for vim mode where we want to go from input mode to normal
 # as fast as possible.
 set -g fish_escape_delay_ms 10
 
-function caly
+# Use ctrn-{n,p,o} to move over history.
+bind --mode insert \cn history-search-backward
+bind --mode insert \cp history-search-forward
+bind --mode insert \co accept-autosuggestion
+
+# Aliases.
+function cc
 	cal --monday --iso --color --year (date '+%Y')
+end
+
+function ll 
+	ls -a --human-readable -l --color=auto --group-directories-first -v $argv
+end
+
+function dd
+	/bin/date "+%Y-%m-%d %H:%M:%ST%z"
 end
