e32565228465b24f0eb75266174d82190b247520
Arturas Lapinskas <arturaslape@gmail.com>
2020-04-05 12:34:45 +0200

    Add keybindings to move around in history.

--- shells/config.fish
+++ shells/config.fish
@@ -4,6 +4,11 @@ if status --is-interactive
 	~/configs/shells/common.bash fish | source
 end
 
+# Use ctrn-{n,p,o} to move over history.
+bind --mode insert \cn history-search-backward
+bind --mode insert \cp history-search-forward
+bind --mode insert \co accept-autosuggestion
+
 # Prevent a delay after escaping.
 # This is useful for vim mode where we want to go from input mode to normal
 # as fast as possible.
