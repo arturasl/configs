8809c56d6e78ff9cc70253e274226b1bb6fea3b3
arturasl <589741+arturasl@users.noreply.github.com>
2024-04-21 17:43:16 +0200

    Tree sitter and some lsp.

--- shells/config.fish
+++ shells/config.fish
@@ -2,35 +2,35 @@ set --export --global CDPATH .
 set --export --global fish_greeting '' # do not show greeting message.
 
 if status --is-interactive
-	~/configs/shells/common.bash fish | source
+    ~/configs/shells/common.bash fish | source
 end
 
 function fish_prompt
-	if [ $status -ne 0 ]
-		set_color red
-	end
-	echo -n '$ '
-	set_color normal
+    if [ $status -ne 0 ]
+        set_color red
+    end
+    echo -n '$ '
+    set_color normal
 end
 
 function fish_user_key_bindings
-	fish_vi_key_bindings
+    fish_vi_key_bindings
 end
 
 function fish_mode_prompt
-	switch $fish_bind_mode
-		case default
-			echo -n N
-		case insert
-			echo -n I
-		case replace-one
-			echo -n R
-		case visual
-			echo -n V
-		case '*'
-			echo -n '?'
-	end
-	echo ' '
+    switch $fish_bind_mode
+        case default
+            echo -n N
+        case insert
+            echo -n I
+        case replace-one
+            echo -n R
+        case visual
+            echo -n V
+        case '*'
+            echo -n '?'
+    end
+    echo ' '
 end
 
 # Prevent a delay after escaping.
@@ -45,13 +45,17 @@ bind --mode insert \co accept-autosuggestion
 
 # Aliases.
 function cc
-	cal --monday --iso --color --year (date '+%Y')
+    cal --monday --iso --color --year (date '+%Y')
 end
 
-function ll 
-	ls -a --human-readable -l --color=auto --group-directories-first -v $argv
+function ll
+    ls -a --human-readable -l --color=auto --group-directories-first -v $argv
 end
 
 function dd
-	/bin/date "+%Y-%m-%d %H:%M:%ST%z"
+    /bin/date "+%Y-%m-%d %H:%M:%ST%z"
+end
+
+function vim
+    nvim $argv
 end
