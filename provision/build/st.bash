9a6fdb50de7dbfbb410a4f2de7f673464d239112
Arturas Lapinskas <arturaslape@gmail.com>
2024-05-01 19:56:41 +0200

    Trying dracula theme.

--- provision/build/st.bash
+++ provision/build/st.bash
@@ -15,8 +15,8 @@ tar xzf "st-${stver}.tar.gz" && mv "st-${stver}"/* . && rm -r "st-${stver}"{,.ta
 # Colors.
 wget "https://st.suckless.org/patches/solarized/st-no_bold_colors-0.8.1.diff" --output-document=st-no_bold_colors.diff
 patch < "st-no_bold_colors.diff"
-wget "https://st.suckless.org/patches/solarized/st-solarized-dark-0.8.5.diff" --output-document=st-solraized-dark.diff
-patch < st-solraized-dark.diff
+wget "https://st.suckless.org/patches/dracula/st-dracula-0.8.5.diff" --output-document=st-dracula.diff
+patch < st-dracula.diff
 
 # Allow to resize window to an arbitrary size.
 wget "https://st.suckless.org/patches/anysize/st-expected-anysize-0.9.diff" --output-document=st-anysize.diff
