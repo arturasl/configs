5af8972668831b00d192450eaaeeb4ded6fd0f0d
arturasl <589741+arturasl@users.noreply.github.com>
2024-05-11 13:17:40 +0200

    Fix Lua lint issues.

--- nvim/init.lua
+++ nvim/init.lua
@@ -118,10 +118,15 @@ require('lazy').setup({
     {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
-        config = function () 
+        config = function ()
             require("nvim-treesitter.configs").setup({
-                -- Recommended, otherwise just depend on auto_install.
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+                -- Required fields, setting to unset to make Lua LSP happy.
+                ensure_installed = { },
+                sync_install = false,
+                ignore_install = {},
+                modules = {},
+                -- Auto install treesitter parser on opening certain file type
+                -- first time.
                 auto_install = true,
                 highlight = {
                     enable = true ,
