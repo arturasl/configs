76c8887f4e7377efed199e2fd473da57890d0f4a
Arturas Lapinskas <arturaslape@gmail.com>
2024-05-12 10:25:42 +0200

    Add explanation for timeout options.

--- nvim/init.lua
+++ nvim/init.lua
@@ -125,6 +125,8 @@ require('lazy').setup({
         'folke/which-key.nvim',
         event = "VeryLazy",
         init = function()
+            -- Allow multi key sequences to cancel after given amount of time.
+            -- Cancelation will trigger which-key.
             vim.o.timeout = true
             vim.o.timeoutlen = 500
         end,
