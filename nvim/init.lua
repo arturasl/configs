07fc65427023fbd313f31a86abb03787a19632d7
Arturas Lapinskas <arturaslape@gmail.com>
2024-05-12 10:46:27 +0200

    Set up basic autocompletion.

--- nvim/init.lua
+++ nvim/init.lua
@@ -209,4 +209,36 @@ require('lazy').setup({
             config.pyright.setup({})
         end
     },
+
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            -- Snippet engine, only using for completion purposes.
+            'hrsh7th/cmp-vsnip',
+            'hrsh7th/vim-vsnip',
+        },
+        config = function()
+            local cmp = require('cmp')
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        vim.fn["vsnip#anonymous"](args.body)
+                    end,
+                },
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'buffer' },
+                    { name = 'path' },
+                }),
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item()),
+                    ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item()),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                }),
+            })
+        end
+    },
 })
