e19c76e7a6bdcbd70ab06bddfb1a081d47725c8a
Arturas Lapinskas <arturaslape@gmail.com>
2024-05-12 12:36:07 +0200

    Highligh colors.

--- nvim/init.lua
+++ nvim/init.lua
@@ -176,6 +176,18 @@ require('lazy').setup({
     },
 
     {
+        'NvChad/nvim-colorizer.lua',
+        config = function()
+            require("colorizer").setup {
+                user_default_options = {
+                    css = true,
+                    mode = "virtualtext",
+                },
+            }
+        end
+    },
+
+    {
         'neovim/nvim-lspconfig',
         dependencies = {
             -- LSP server installer.
