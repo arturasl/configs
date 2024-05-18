65e17966192eb0bdaf2d754af74562d3b96b3039
arturasl <589741+arturasl@users.noreply.github.com>
2024-05-18 14:54:08 +0200

    Root telescope searching.

--- nvim/lua/custom/functions.lua
+++ nvim/lua/custom/functions.lua
@@ -14,11 +14,30 @@ return {
             vim.cmd("copen")
         end
     end,
+
     open_quick_fix_if_not_empty = function()
         if not vim.tbl_isempty(vim.fn.getqflist()) then
             vim.cmd("copen")
         else
             vim.cmd("cclose")
         end
+    end,
+
+    find_root = function()
+        local root_files = { "Cargo.toml", ".git", ".svn" }
+        for _, root_file in ipairs(root_files) do
+            local found = ""
+            if found == "" then
+                found = vim.fn.findfile(root_file, ".;") or ""
+            end
+            if found == "" then
+                found = vim.fn.finddir(root_file, ".;") or ""
+            end
+            if found ~= "" then
+                return vim.fs.dirname(found)
+            end
+        end
+
+        return vim.fn.expand("%:p:h")
     end,
 }
--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -1,9 +1,19 @@
 return {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
     config = function()
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", ",ff", builtin.live_grep, { desc = "Live grep" })
+
+        vim.keymap.set("n", ",ff", function()
+            builtin.find_files({ cwd = require("custom/functions").find_root() })
+        end, { desc = "Find files" })
+
+        vim.keymap.set("n", ",fg", function()
+            builtin.live_gre({ cwd = require("custom/functions").find_root() })
+        end, { desc = "Live grep" })
+
         vim.keymap.set("n", ",fh", builtin.oldfiles, { desc = "Previous files" })
     end,
 }
