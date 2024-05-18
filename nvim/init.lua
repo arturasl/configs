2131d5d3ad1bc59503c1c9c25246b34fd2707903
Arturas Lapinskas <arturaslape@gmail.com>
2024-05-18 13:36:54 +0200

    Move vcs settings to a separate file.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,3 @@
 require("custom/options")
 require("custom/lazy")
-
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "gitcommit", "hgcommit" },
-    group = vim.api.nvim_create_augroup("ft_vcs", { clear = true }),
-    callback = function()
-        -- Autowrap at 80 characters.
-        vim.opt_local.textwidth = 80
-        vim.opt_local.formatoptions:append("t")
-    end,
-})
+require("custom/ft/vcs")
