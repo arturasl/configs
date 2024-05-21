04bcfaa579e75bd90439a7071572105426483d35
Arturas Lapinskas <arturaslape@gmail.com>
2024-05-21 20:49:34 +0200

    Change bufstop mapping to <space>[c]ahnge.

--- nvim/lua/custom/plugins/buffstop.lua
+++ nvim/lua/custom/plugins/buffstop.lua
@@ -4,6 +4,6 @@ return {
         vim.g.BufstopKeys = "asdfghjkl"
     end,
     config = function()
-        vim.keymap.set("n", "<space>b", ":BufstopModeFast<cr>")
+        vim.keymap.set("n", "<space>c", ":BufstopModeFast<cr>")
     end,
 }
