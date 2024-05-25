d95de67cf3e40e9473bfa66a73a7f21192fe5f3d
Arturas Lapinskas <arturaslape@gmail.com>
2024-05-25 09:48:01 +0200

    Add command line completion.

--- nvim/lua/custom/plugins/completion.lua
+++ nvim/lua/custom/plugins/completion.lua
@@ -4,15 +4,18 @@ return {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
     },
     config = function()
         local cmp = require("cmp")
+
         cmp.setup({
             snippet = {
                 expand = function(args)
                     vim.snippet.expand(args.body)
                 end,
             },
+
             sources = cmp.config.sources(
                 -- Priority group 1.
                 {
@@ -24,6 +27,7 @@ return {
                     { name = "buffer" },
                 }
             ),
+
             mapping = cmp.mapping.preset.insert({
                 ["<c-n>"] = cmp.mapping(cmp.mapping.select_next_item()),
                 ["<c-p>"] = cmp.mapping(cmp.mapping.select_prev_item()),
@@ -31,6 +35,24 @@ return {
                 ["<c-f>"] = cmp.mapping.scroll_docs(4),
                 ["<cr>"] = cmp.mapping.confirm({ select = true }),
             }),
+        })
+
+        cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources(
+                -- Priority group 1.
+                {
+                    { name = "path" },
+                },
+                -- Priority group 2.
+                {
+                    {
+                        name = "cmdline",
+                        keyword_length = 2,
+                    },
+                }
+            ),
+            matching = { disallow_symbol_nonprefix_matching = false },
         })
     end,
 }
