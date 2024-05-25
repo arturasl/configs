826337346ddb8f247552fee5c6dd6b9e826ccade
arturasl <589741+arturasl@users.noreply.github.com>
2024-05-25 09:36:30 +0200

    Move to nerd fonts.

--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
     },
     config = function()
         local builtin = require("telescope.builtin")
