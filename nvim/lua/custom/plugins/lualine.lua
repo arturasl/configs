52bbd69f181e863c104e4f7e460c01bce318462c
arturasl <589741+arturasl@users.noreply.github.com>
2024-05-29 21:33:39 +0200

    Include selection count and move common sections to a separate variable.

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -1,23 +1,18 @@
 return {
     "nvim-lualine/lualine.nvim",
     config = function()
+        local per_window_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { "filename" },
+            lualine_x = { "" },
+            lualine_y = { "" },
+            lualine_z = { "" },
+        }
+
         require("lualine").setup({
-            inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = { "filename" },
-                lualine_x = { "" },
-                lualine_y = { "" },
-                lualine_z = { "" },
-            },
-            sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = { "filename" },
-                lualine_x = { "" },
-                lualine_y = { "" },
-                lualine_z = { "" },
-            },
+            inactive_sections = per_window_sections,
+            sections = per_window_sections,
             tabline = {
                 lualine_a = { "mode" },
                 lualine_b = { "diagnostics" },
@@ -31,8 +26,8 @@ return {
                     },
                 },
                 lualine_x = { "encoding", "fileformat", "filetype" },
-                lualine_y = { "progress" },
-                lualine_z = { "location" },
+                lualine_y = { "progress", "location", "selectioncount" },
+                lualine_z = {},
             },
         })
     end,
