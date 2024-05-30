61880ee63462d1272de46057fc8b18ae4abf4f03
arturasl <589741+arturasl@users.noreply.github.com>
2024-05-30 08:34:10 +0200

    Remove bufstop.

--- nvim/lua/custom/plugins/buffstop.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "mihaifm/bufstop",
-    init = function()
-        vim.g.BufstopKeys = "asdfghjkl"
-    end,
-    config = function()
-        vim.keymap.set("n", "<space>v", "<cmd>BufstopModeFast<cr>")
-    end,
-}
