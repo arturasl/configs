55b58299b2450549e54b8cf825bb49cbb12209a8
arturasl <589741+arturasl@users.noreply.github.com>
2024-05-31 20:35:31 +0200

    Update mason managed binaries after plugin update.

--- nvim/lua/custom/plugins/mason.lua
+++ nvim/lua/custom/plugins/mason.lua
@@ -1,6 +1,7 @@
 return {
     -- Installs LSP servers, formatters, linters, etc.
     "williamboman/mason.nvim",
+    build = ":MasonUpdate",
     config = function()
         require("mason").setup()
     end,
