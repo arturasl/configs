4aa6ea69d83bafb3abfd070e11a84ce8fe2c57e5
arturasl <589741+arturasl@users.noreply.github.com>
2024-06-02 10:47:44 +0200

    New autoclose/endwise/autotag plugins.

--- nvim/lua/custom/plugins/autoclose.lua
+++ nvim/lua/custom/plugins/autoclose.lua
@@ -1,8 +1,23 @@
 return {
-    -- * auto-pairs:
-    --     finishes quotation marks, braces, parenthesis, etc.
-    -- * endwise:
-    --     finishes "blocks" like for `function() <cr>` will automatically
-    --     insert `end`.
-    "cohama/lexima.vim",
+    -- Finishes quotation marks, braces, parenthesis, etc.
+    {
+        "windwp/nvim-autopairs",
+        config = function()
+            require("nvim-autopairs").setup({ disable_in_macro = false })
+        end,
+    },
+
+    -- Finishes xml tags `<div>` will automatically insert `</div>`.
+    {
+        "windwp/nvim-ts-autotag",
+        config = function()
+            require("nvim-ts-autotag").setup()
+        end,
+    },
+
+    -- Finishes "blocks" like patterns, e.g. for `function() <cr>` will
+    -- automatically insert `end`.
+    {
+        "tpope/vim-endwise",
+    },
 }
