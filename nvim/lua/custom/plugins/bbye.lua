159628d6a02ca750d1c289be5234cd9e6f78cc2e
Arturas Lapinskas <arturaslape@gmail.com>
2024-07-13 09:52:44 +0200

    Completely move buffer closing maps to bbye.

--- nvim/lua/custom/plugins/bbye.lua
+++ nvim/lua/custom/plugins/bbye.lua
@@ -1,6 +1,6 @@
 return {
     "moll/vim-bbye",
     config = function()
-        vim.keymap.set("n", "<space>q", "<cmd>Bdelete<cr>", { desc = "Close current buffer" })
+        vim.keymap.set("n", "<space>q", "<cmd>Bdelete<cr>", { desc = "Buffer [Q]uit (bbye)" })
     end,
 }
