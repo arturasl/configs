8bbedcd774c902a3740e0dc664f8a1f56c87b510
arturasl <589741+arturasl@users.noreply.github.com>
2024-08-04 18:15:32 +0200

    Try flash.

--- nvim/lua/custom/plugins/indent-object.lua
+++
@@ -1,7 +0,0 @@
-return {
-    -- Additional text object for action based on indentation level.
-    -- vii -- visual inside current indentation block;
-    -- vai -- visual inside current block + line before;
-    -- vaI -- visual inside current block + line before + line after;
-    "michaeljsmith/vim-indent-object",
-}
