e37833c69f7a88116c1accb8f457224e74b2d4f9
Arturas Lapinskas <arturaslape@gmail.com>
2024-08-04 18:15:32 +0200

    Try flash.

---
+++ nvim/lua/custom/plugins/moving.lua
@@ -0,0 +1,32 @@
+return {
+    {
+        -- Additional text object for action based on indentation level.
+        -- vii -- visual inside current indentation block;
+        -- vai -- visual inside current block + line before;
+        -- vaI -- visual inside current block + line before + line after;
+        "michaeljsmith/vim-indent-object",
+    },
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            {
+                "s",
+                mode = { "n", "x", "o" },
+                function()
+                    require("flash").jump()
+                end,
+                desc = "Flash",
+            },
+            {
+                "S",
+                mode = { "n", "x", "o" },
+                function()
+                    require("flash").treesitter()
+                end,
+                desc = "Flash Treesitter",
+            },
+        },
+    },
+}
