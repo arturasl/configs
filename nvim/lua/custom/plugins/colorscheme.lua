dc699acfca07e2600110fed8e38c5623e189e88a
arturasl <589741+arturasl@users.noreply.github.com>
2025-01-11 20:02:20 +0100

    Try tokyonight.

--- nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -1,7 +1,16 @@
 return {
     "rebelot/kanagawa.nvim",
     priority = 1000, -- Load first.
+    dependencies = {
+        "folke/tokyonight.nvim",
+    },
     config = function()
-        vim.cmd.colorscheme("kanagawa")
+        local schemes = {
+            "tokyonight-storm",
+            "tokyonight-moon",
+            "kanagawa",
+        }
+        local weeks_since_epoch = math.floor(os.time() / (60 * 60 * 24 * 7))
+        vim.cmd.colorscheme(schemes[weeks_since_epoch % #schemes + 1])
     end,
 }
