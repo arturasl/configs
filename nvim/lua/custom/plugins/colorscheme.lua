f5567301f9ff6e410da2ea9a10452a11ce78b540
arturasl <589741+arturasl@users.noreply.github.com>
2025-01-12 18:30:42 +0100

    Add fox colorschemes.

--- nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -3,12 +3,17 @@ return {
     priority = 1000, -- Load first.
     dependencies = {
         "folke/tokyonight.nvim",
+        "EdenEast/nightfox.nvim",
     },
     config = function()
         local schemes = {
-            "tokyonight-storm",
-            "tokyonight-moon",
+            "carbonfox",
+            "duskfox",
             "kanagawa",
+            "nightfox",
+            "terafox",
+            "tokyonight-moon",
+            "tokyonight-storm",
         }
         local weeks_since_epoch = math.floor(os.time() / (60 * 60 * 24 * 7))
         vim.cmd.colorscheme(schemes[weeks_since_epoch % #schemes + 1])
