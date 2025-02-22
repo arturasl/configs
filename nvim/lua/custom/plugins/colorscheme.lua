3ff1c44a35ade09024d7ea2486adfc02293635ed
Arturas Lapinskas <arturaslape@gmail.com>
2025-02-22 15:09:34 +0100

    Add todo coloring.

--- nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -1,25 +1,34 @@
 return {
-    "rebelot/kanagawa.nvim",
-    priority = 1000, -- Load first.
-    dependencies = {
-        "folke/tokyonight.nvim",
-        "EdenEast/nightfox.nvim",
-        "catppuccin/nvim",
+    {
+        "rebelot/kanagawa.nvim",
+        priority = 1000, -- Load first.
+        dependencies = {
+            "folke/tokyonight.nvim",
+            "EdenEast/nightfox.nvim",
+            "catppuccin/nvim",
+        },
+        config = function()
+            local schemes = {
+                "carbonfox",
+                "duskfox",
+                "kanagawa",
+                "nightfox",
+                "terafox",
+                "tokyonight-moon",
+                "tokyonight-storm",
+                "catppuccin-macchiato",
+                "catppuccin-mocha",
+                "catppuccin-frappe",
+            }
+            local weeks_since_epoch = math.floor(os.time() / (60 * 60 * 24 * 7))
+            vim.cmd.colorscheme(schemes[weeks_since_epoch % #schemes + 1])
+        end,
     },
-    config = function()
-        local schemes = {
-            "carbonfox",
-            "duskfox",
-            "kanagawa",
-            "nightfox",
-            "terafox",
-            "tokyonight-moon",
-            "tokyonight-storm",
-            "catppuccin-macchiato",
-            "catppuccin-mocha",
-            "catppuccin-frappe",
-        }
-        local weeks_since_epoch = math.floor(os.time() / (60 * 60 * 24 * 7))
-        vim.cmd.colorscheme(schemes[weeks_since_epoch % #schemes + 1])
-    end,
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("todo-comments").setup({})
+        end,
+    },
 }
