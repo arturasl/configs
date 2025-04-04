2a521ddaf37abd5efe8e237accb4a9a17e298f8f
Arturas Lapinskas <arturaslape@gmail.com>
2025-04-04 18:17:18 +0200

    Always load tiny inline diagnostics (ts not lazy).

--- nvim/lua/custom/plugins/tiny_inline_diagnostic.lua
+++ nvim/lua/custom/plugins/tiny_inline_diagnostic.lua
@@ -1,6 +1,5 @@
 return {
     "rachartier/tiny-inline-diagnostic.nvim",
-    event = "VeryLazy",
     priority = 1000,
     config = function()
         vim.diagnostic.config({ virtual_text = false })
