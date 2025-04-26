aaa6be9a660a674ac0a2a49033a405c6d387e3cf
Arturas Lapinskas <arturaslape@gmail.com>
2025-04-26 10:01:18 +0200

    Guess the delimiter.

--- nvim/lua/custom/plugins/ft.lua
+++ nvim/lua/custom/plugins/ft.lua
@@ -4,7 +4,32 @@ return {
         local csvview = require("csvview")
         csvview.setup({
             parser = {
-                delimiter = ";",
+                delimiter = function(bufnr)
+                    local first_line = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ""
+
+                    local char_hist = {}
+                    for ch in first_line:gmatch(".") do
+                        char_hist[ch] = (char_hist[ch] or 0) + 1
+                    end
+
+                    local sorted_hist = {}
+                    for k, v in pairs(char_hist) do
+                        table.insert(sorted_hist, { k, v })
+                    end
+                    table.sort(sorted_hist, function(lhs, rhs)
+                        return lhs[1] < rhs[1]
+                    end)
+
+                    local delimiters = { [","] = true, [";"] = true, ["|"] = true }
+                    for _, vk in pairs(sorted_hist) do
+                        local ch = vk[1]
+                        if delimiters[ch] then
+                            return ch
+                        end
+                    end
+
+                    return ","
+                end,
             },
             view = {
                 header_lnum = 1,
