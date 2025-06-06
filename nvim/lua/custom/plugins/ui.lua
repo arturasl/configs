5698315f1d4416295f52757d7f5cc4d9bb9146da
Arturas Lapinskas <arturaslape@gmail.com>
2025-06-06 17:31:13 +0200

    Try smearing.

--- nvim/lua/custom/plugins/ui.lua
+++ nvim/lua/custom/plugins/ui.lua
@@ -1,10 +1,27 @@
 return {
-    "stevearc/dressing.nvim",
-    config = function()
-        require("dressing").setup({
-            select = {
-                backend = { "builtin" },
-            },
-        })
-    end,
+    {
+        "stevearc/dressing.nvim",
+        config = function()
+            require("dressing").setup({
+                select = {
+                    backend = { "builtin" },
+                },
+            })
+        end,
+    },
+
+    {
+        "sphamba/smear-cursor.nvim",
+        config = function()
+            require("smear_cursor").setup({
+                -- Faster smear.
+                stiffness = 0.8,
+                trailing_stiffness = 0.5,
+                distance_stop_animating = 0.5,
+                -- Do not jump to cmd (after pressing `:`).
+                -- BUG: https://github.com/sphamba/smear-cursor.nvim/issues/78
+                smear_to_cmd = false,
+            })
+        end,
+    },
 }
