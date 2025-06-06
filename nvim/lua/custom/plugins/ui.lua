0c1c9d9a7c61e91284b61a60feea8d1ba80fcd7e
Arturas Lapinskas <arturaslape@gmail.com>
2025-06-06 17:33:34 +0200

    Neoscroll.

--- nvim/lua/custom/plugins/ui.lua
+++ nvim/lua/custom/plugins/ui.lua
@@ -24,4 +24,13 @@ return {
             })
         end,
     },
+
+    {
+        "karb94/neoscroll.nvim",
+        config = function()
+            require("neoscroll").setup({
+                duration_multiplier = 0.3,
+            })
+        end,
+    },
 }
