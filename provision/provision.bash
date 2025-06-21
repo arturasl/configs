0499b87df08abf5dfcdea656c9c049fc0a5456ae
arturasl <589741+arturasl@users.noreply.github.com>
2025-06-21 22:50:26 +0200

    Also install setuptools.

--- provision/provision.bash
+++ provision/provision.bash
@@ -15,7 +15,7 @@ sudo pacman -S git subversion mercurial # Version control
 sudo pacman -S zeal # Offline help docs.
 
 ## Language specific {{
-sudo pacman -S python python-pip uv # Python
+sudo pacman -S python python-pip python-setuptools uv # Python
 sudo pacman -S fpc # Pascal
 sudo pacman -S nodejs npm # JavaScript
 sudo pacman -S lua lua51 luarocks # Lua
