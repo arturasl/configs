6d0f29643ee520c40e755789ce33f4acc0eb4c36
arturasl <589741+arturasl@users.noreply.github.com>
2025-06-21 21:16:11 +0200

    Update provision for arch and clean up unused packages.

--- provision/build/git-gnome-keyring.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -xe
-
-cd /usr/share/doc/git/contrib/credential/gnome-keyring
-sudo make
-git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
