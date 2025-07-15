3aec8713cb7e39bf4f5109a6eeac94c925011059
arturasl <589741+arturasl@users.noreply.github.com>
2025-07-15 21:26:33 +0200

    Use OSC52 for copying (and expand tmux to passthrough more things).

--- scripts/clipboard.bash
+++ scripts/clipboard.bash
@@ -2,22 +2,20 @@
 
 source "$(dirname "$0")/util.bash"
 
-if [ "$1" = '--paste' ]; then
-	if utilCommandExists 'pbpaste' ; then
-		pbpaste
-	elif utilCommandExists 'xclip' ; then
-		xclip -o -selection clipboard
-	else
-		utilShowError "Could not find program for pasting"
-	fi
-elif [ "$1" = '--copy' ]; then
-	if utilCommandExists 'pbcopy' ; then
-		pbcopy
-	elif utilCommandExists 'xclip' ; then
-		xclip -i -selection clipboard
-	else
-		utilShowError "Could not find program for pasting"
-	fi
+tty=/dev/stdout
+# Within tmux use parent tty.
+[ -n "$TMUX" ] && tty="$(tmux display-message -p '#{client_tty}')"
+
+if [[ "$1" = '--paste' ]]; then
+    if utilCommandExists 'pbpaste' ; then
+        pbpaste
+    elif utilCommandExists 'xclip' ; then
+        xclip -o -selection clipboard
+    else
+        utilShowError "Could not find program for pasting"
+    fi
+elif [[ "$1" = '--copy' ]]; then
+    printf $'\e]52;c;%s\a' "$(base64 | tr -d '\n')" > "$tty"
 else
-	utilShowError "Unknown parameter: \"${1}\""
+    utilShowError "Unknown parameter: \"${1}\""
 fi
