92764eb372287a9c38ca141ebce2a1e6ad0e1e84
arturasl <589741+arturasl@users.noreply.github.com>
2025-08-01 12:40:04 +0200

    Use default csv delimiter detection.

--- nvim/lua/custom/plugins/ft.lua
+++ nvim/lua/custom/plugins/ft.lua
@@ -1,37 +1,3 @@
-local function guess_delimiter(bufnr)
-    local first_line = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ""
-
-    local char_hist = {}
-    for ch in first_line:gmatch(".") do
-        char_hist[ch] = (char_hist[ch] or 0) + 1
-    end
-
-    local sorted_hist = {}
-    for k, v in pairs(char_hist) do
-        table.insert(sorted_hist, { k, v })
-    end
-    table.sort(sorted_hist, function(lhs, rhs)
-        if lhs[1] ~= rhs[1] then
-            return lhs[1] < rhs[1]
-        end
-        return lhs[2] < lhs[2]
-    end)
-
-    local delimiters = {}
-    for ch in (",;|\t"):gmatch(".") do
-        delimiters[ch] = true
-    end
-
-    for _, vk in pairs(sorted_hist) do
-        local ch = vk[1]
-        if delimiters[ch] then
-            return ch
-        end
-    end
-
-    return ","
-end
-
 return {
     "hat0uma/csvview.nvim",
     ft = "csv",
@@ -39,10 +5,11 @@ return {
         local csvview = require("csvview")
         csvview.setup({
             parser = {
-                delimiter = guess_delimiter,
+                delimiter = {
+                    ft = {},
+                },
             },
             view = {
-                header_lnum = 1,
                 display_mode = "border",
             },
             keymaps = {
