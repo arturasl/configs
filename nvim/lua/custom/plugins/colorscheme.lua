5c50c04d9c505dd90de1cf57e6532b4c10b728f4
arturasl <589741+arturasl@users.noreply.github.com>
2025-10-28 20:58:41 +0100

    Initialize color schemes lazily.

--- nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -1,49 +1,109 @@
-return {
+local M = {}
+
+local color_scheme_plugins = {
     {
-        "rebelot/kanagawa.nvim",
-        priority = 1000, -- Load first.
-        dependencies = {
-            "folke/tokyonight.nvim",
-            "EdenEast/nightfox.nvim",
-            "catppuccin/nvim",
-            "Mofiqul/dracula.nvim",
-            "AlexvZyl/nordic.nvim",
-            "bluz71/vim-nightfly-colors",
-            "scottmckendry/cyberdream.nvim",
-            "marko-cerovac/material.nvim",
-            "vague2k/vague.nvim",
+        name = "rebelot/kanagawa.nvim",
+        provides = {
+            "kanagawa",
         },
-        config = function()
-            local schemes = {
-                "carbonfox",
-                "duskfox",
-                "kanagawa",
-                "nightfox",
-                "terafox",
-                "tokyonight-moon",
-                "tokyonight-storm",
-                "catppuccin-macchiato",
-                "catppuccin-mocha",
-                "catppuccin-frappe",
-                "dracula",
-                "nordic",
-                "nightfly",
-                "cyberdream",
-                "material-deep-ocean",
-                "material-darker",
-                "material-palenight",
-                "vague",
-            }
-            local days_since_epoch = math.floor(os.time() / (60 * 60 * 24))
-            assert(7 % #schemes ~= 0)
-            vim.cmd.colorscheme(schemes[(days_since_epoch * 7) % #schemes + 1])
-        end,
     },
     {
-        "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("todo-comments").setup({})
-        end,
+        name = "folke/tokyonight.nvim",
+        provides = {
+            "tokyonight-moon",
+            "tokyonight-storm",
+        },
+    },
+    {
+        name = "EdenEast/nightfox.nvim",
+        provides = {
+            "carbonfox",
+            "duskfox",
+            "nightfox",
+            "terafox",
+        },
+    },
+    {
+        name = "catppuccin/nvim",
+        provides = {
+            "catppuccin-macchiato",
+            "catppuccin-mocha",
+            "catppuccin-frappe",
+        },
+    },
+    {
+        name = "Mofiqul/dracula.nvim",
+        provides = {
+            "dracula",
+        },
+    },
+    {
+        name = "AlexvZyl/nordic.nvim",
+        provides = {
+            "nordic",
+        },
+    },
+    {
+        name = "bluz71/vim-nightfly-colors",
+        provides = {
+            "nightfly",
+        },
+    },
+    {
+        name = "scottmckendry/cyberdream.nvim",
+        provides = {
+            "cyberdream",
+        },
+    },
+    {
+        name = "marko-cerovac/material.nvim",
+        provides = {
+            "material-deep-ocean",
+            "material-darker",
+            "material-palenight",
+        },
+    },
+    {
+        name = "vague2k/vague.nvim",
+        provides = {
+            "vague",
+        },
     },
 }
+
+local linearized_schemes = {}
+for _, plugin in ipairs(color_scheme_plugins) do
+    for _, scheme in ipairs(plugin.provides) do
+        linearized_schemes[#linearized_schemes + 1] = {
+            plugin_name = plugin.name,
+            scheme_name = scheme,
+        }
+    end
+end
+local days_since_epoch = math.floor(os.time() / (60 * 60 * 24))
+assert(7 % #linearized_schemes ~= 0)
+local picked_scheme = linearized_schemes[(days_since_epoch * 7) % #linearized_schemes + 1]
+
+for _, plugin in ipairs(color_scheme_plugins) do
+    local add = { plugin.name }
+    if picked_scheme.plugin_name == plugin.name then
+        add.priority = 1000
+        add.config = function()
+            vim.cmd.colorscheme(picked_scheme.scheme_name)
+        end
+    else
+        add.lazy = true
+    end
+
+    M[#M + 1] = add
+end
+
+M[#M + 1] = {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        require("todo-comments").setup({})
+    end,
+}
+
+return M
