59a0b7c35b9004c55cdc73c41244d5db8a5ac712
arturasl <589741+arturasl@users.noreply.github.com>
2025-10-28 21:06:21 +0100

    Lazily load bbye and undotree.

--- nvim/lua/custom/plugins/bbye.lua
+++ nvim/lua/custom/plugins/bbye.lua
@@ -1,6 +1,6 @@
 return {
     "moll/vim-bbye",
-    config = function()
-        vim.keymap.set("n", "<space>q", "<cmd>Bdelete<cr>", { desc = "Buffer [Q]uit (bbye)" })
-    end,
+    keys = {
+        { "<space>q", "<cmd>Bdelete<cr>", desc = "Buffer [Q]uit (bbye)" },
+    },
 }
