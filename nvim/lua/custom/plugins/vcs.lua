fbfe2ada9e2eb8afb4194ca577d4f838efa1d820
arturasl <589741+arturasl@users.noreply.github.com>
2025-10-28 21:03:45 +0100

    Initialize vcs plugins lazily.

--- nvim/lua/custom/plugins/vcs.lua
+++ nvim/lua/custom/plugins/vcs.lua
@@ -1,9 +1,12 @@
 return {
     {
         "mhinz/vim-signify",
+        event = "VeryLazy",
     },
+
     {
         "rafikdraoui/jj-diffconflicts",
+        cmd = "JJDiffConflicts",
         init = function()
             vim.g.jj_diffconflicts_show_usage_message = false
         end,
