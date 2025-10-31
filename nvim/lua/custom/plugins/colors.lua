0774088c97de3c8d39d8cc0e3dcac3b5448135f7
arturasl <589741+arturasl@users.noreply.github.com>
2025-10-31 21:51:12 +0100

    Merge colorize and colorscheme -> colors.

--- nvim/lua/custom/plugins/colors.lua
+++ nvim/lua/custom/plugins/colors.lua
@@ -107,4 +107,18 @@ M[#M + 1] = {
     end,
 }
 
+M[#M + 1] = {
+    "NvChad/nvim-colorizer.lua",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        require("colorizer").setup({
+            user_default_options = {
+                css = true,
+                mode = "virtualtext",
+                names = false, -- Disable showing colors for names like "black".
+            },
+        })
+    end,
+}
+
 return M
