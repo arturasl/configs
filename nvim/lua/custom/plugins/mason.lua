82a066e456e861d9f8b7a999e8dae46adb72df2f
arturasl <589741+arturasl@users.noreply.github.com>
2025-10-31 21:55:19 +0100

    Make mason update all a more direct dependency of the mason.

--- nvim/lua/custom/plugins/mason.lua
+++ nvim/lua/custom/plugins/mason.lua
@@ -1,20 +1,17 @@
 return {
     {
-        "RubixDev/mason-update-all",
-        config = function()
-            require("mason-update-all").setup({
-                show_no_updates_notification = false,
-            })
-        end,
-    },
-
-    {
         -- Installs LSP servers, formatters, linters, etc.
         "mason-org/mason.nvim",
         build = ":MasonUpdateAll",
         dependencies = {
-            -- For `:MasonUpdateAll` command.
-            "RubixDev/mason-update-all",
+            {
+                "RubixDev/mason-update-all",
+                config = function()
+                    require("mason-update-all").setup({
+                        show_no_updates_notification = false,
+                    })
+                end,
+            },
         },
         config = function()
             require("mason").setup()
