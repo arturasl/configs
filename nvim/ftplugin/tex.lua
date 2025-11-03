ad26ffe31cfd5c1b7ff434b855ad93fda59454ad
arturasl <589741+arturasl@users.noreply.github.com>
2025-11-03 13:11:31 +0100

    Migrate to latexmk.

--- nvim/ftplugin/tex.lua
+++ nvim/ftplugin/tex.lua
@@ -5,15 +5,17 @@ vim.opt_local.errorformat = "%f:%l: %m"
 run_cmd_on_key({
     fn_cmd = function()
         return {
-            "pdflatex",
-            "-shell-escape",
-            "-file-line-error",
-            "-interaction=nonstopmode",
-            "-halt-on-erro",
+            -- Similar to pdflatex, but runs multiple times to resolve any
+            -- dependencies (e.g. ensures page number is never `??`).
+            "latexmk",
+            "-pdf",
             vim.fn.expand("%:p"),
         }
     end,
     onsuccess = function()
+        -- Remove intermediate files.
+        vim.cmd("silent !latexmk -c '%:p' &>/dev/null &")
+        -- Open pdf in the default viewer.
         vim.cmd("silent !~/configs/scripts/showme.bash --silent-detached '%:p:r.pdf' &>/dev/null &")
     end,
     desc = "[B]uild LaTeX",
