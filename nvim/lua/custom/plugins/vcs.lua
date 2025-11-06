11865a6a1b698102b58e766db5d61e20d7f43a2c
arturasl <589741+arturasl@users.noreply.github.com>
2025-11-06 20:26:14 +0100

    Use custom signs for vim-signify.

--- nvim/lua/custom/plugins/vcs.lua
+++ nvim/lua/custom/plugins/vcs.lua
@@ -1,7 +1,13 @@
 return {
     {
         "mhinz/vim-signify",
-        event = "VeryLazy",
+        event = { "BufReadPre" },
+        init = function()
+            vim.g.signify_sign_add = "▎"
+            vim.g.signify_sign_delete = "▎"
+            vim.g.signify_sign_delete_first_line = "▎"
+            vim.g.signify_sign_change = "▎"
+        end,
     },
 
     {
