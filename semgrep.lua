958be4e0f4ef7395866a9e241f94f60b55877edf
arturasl <589741+arturasl@users.noreply.github.com>
2026-01-18 09:23:23 +0100

    Add a check for usage of `/` in require.

--- semgrep.lua
+++ semgrep.lua
@@ -1,5 +1,6 @@
 -- To run use:
 --   semgrep scan --test --config semgrep.yaml semgrep.lua
+local vim = {}
 
 -- ruleid: avoid-vim-o
 vim.o.relativenumber = true
@@ -9,10 +10,16 @@ vim.opt.relativenumber = true
 
 -- ruleid: always-include-auto-group-name
 vim.api.nvim_create_autocmd("FileType", {
-    callback = function() end,
+	callback = function() end,
 })
 
 -- ok: always-include-auto-group-name
 vim.api.nvim_create_autocmd("FileType", {
-    group = vim.api.nvim_create_augroup("treesitter_init", { clear = true }),
+	group = vim.api.nvim_create_augroup("treesitter_init", { clear = true }),
 })
+
+-- ruleid: use-dot-for-modules
+require("a/b/c")
+--
+-- ok: use-dot-for-modules
+require("a.b.c")
