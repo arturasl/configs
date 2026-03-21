6ea6f87b814990a26efc6987167c249e086e8910
arturasl <589741+arturasl@users.noreply.github.com>
2026-03-21 19:35:08 +0100

    Add DE blurring extension.

--- provision/provision.bash
+++ provision/provision.bash
@@ -167,6 +167,7 @@ sudo pacman -S gnome-browser-connector
 # https://extensions.gnome.org/extension/5660/weather-or-not/
 # https://extensions.gnome.org/extension/9346/maximize-to-workspace-with-history/
 # https://extensions.gnome.org/extension/7855/dash-in-panel/
+# https://extensions.gnome.org/extension/3193/blur-my-shell/
 # }}}
 
 #}}}
