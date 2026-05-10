bd97cc2f19e5e9e402fe0ea73719b4e529cf9245
arturasl <589741+arturasl@users.noreply.github.com>
2026-05-10 11:42:32 +0200

    Simplify information shown by `time`.

--- nvim/ftplugin/haskell.lua
+++ nvim/ftplugin/haskell.lua
@@ -6,7 +6,7 @@ if cabal_project ~= "" then
     local pkg_name = vim.fn.fnamemodify(cabal_project, ":h:r")
     run_cmd_on_key({
         fn_cmd = function()
-            return { "time", "cabal", "run", pkg_name }
+            return { "/usr/bin/time", "-f", "\n---\nElapsed: %E", "cabal", "run", pkg_name }
         end,
         pipe_first_known_file = { "./in", "./small.in", "./large.in" },
         desc = "Build & [R]un Cabal",
